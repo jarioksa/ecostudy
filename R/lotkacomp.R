6fa361cf9159ba2cf4c4519fba5f19ce76aa5224
Jari Oksanen <jari.oksanen@oulu.fi>
2012-02-27 16:31:04 +0200

    Add lines.lotkacomp
    lines.lotkacomp() draws population trajectories to a lotkacomp plot.
    It uses Hank Stevens's primer package to find the trajectories via
    numerical integration, and ecostudy now depends on primer.

--- R/lotkacomp.R
+++ R/lotkacomp.R
@@ -40,3 +40,16 @@
         arrows(0, py, x$alpha * (x$K1/x$alpha - py), py, col = "red")
     }
 }
+
+`lines.lotkacomp` <-
+    function(x, N1 = 1, N2 = 1, r1 = 0.2, r2 = 0.2, ...)
+{
+    parms <- c(r1 = r1, r2 = r2, a11 = 1/x$K1, a22 = 1/x$K2,
+               a12 = x$alpha/x$K1, a21 = x$beta/x$K2)
+    initialN <- c(N1, N2)
+    out <- ode(y = initialN, times = 1:100, func = lvcomp2,
+               parms = parms)
+    out <- out[,-1]
+    lines(out, ...)
+    invisible(out)
+}
