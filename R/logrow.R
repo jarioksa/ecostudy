5fe1a05170c4ca2f35c82973bb87808c890091f8
Jari Oksanen <jari.oksanen@oulu.fi>
2012-02-27 13:42:16 +0200

    Make a valid package
    Add rudiments of documentation and make to use modern package
    structure.

--- R/logrow.R
+++ R/logrow.R
@@ -1,13 +1,14 @@
-"logrow" <-
-  function(N0,b,d,K,bcontr=0.5,out=0) {
+`logrow` <-
+  function(N0, b, d, K, bcontr=0.5, out=0)
+{
     c <- (K-N0)/N0
     r <- b-d
     t <- 0
     N <- N0
     while(N[t+1] < 0.998*K) {
-      t <- t+1
-      tmp <- K/(1+c*exp(-r*t))
-      N <- c(N,tmp)
+        t <- t+1
+        tmp <- K/(1+c*exp(-r*t))
+        N <- c(N,tmp)
     }
     tend <- length(N)
     t <- seq(0,length(N)-1)
@@ -18,26 +19,26 @@
     simN <- N0
     size <- N0
     while (now < tend) {
-      adj <- (b-d)*(size/K)
-      bN <- b - bcontr*adj
-      dN <- d + (1-bcontr)*adj
-      events <- (bN+dN)*size
-      step <- rexp(1,events)
-      now <- now+step
-      if (runif(1) <= bN/(bN+dN)) {
-        size <- size+1
-      }
-      else {
-        size <- size-1
-      }
-      simN <- c(simN,size)
-      time <- c(time,now)
-      if (size == 0) break
+        adj <- (b-d)*(size/K)
+        bN <- b - bcontr*adj
+        dN <- d + (1-bcontr)*adj
+        events <- (bN+dN)*size
+        step <- rexp(1,events)
+        now <- now+step
+        if (runif(1) <= bN/(bN+dN)) {
+            size <- size+1
+        }
+        else {
+            size <- size-1
+        }
+        simN <- c(simN,size)
+        time <- c(time,now)
+        if (size == 0) break
     }
     lines(time,simN,col="blue")
     final <- cbind(time,simN)
     if (out != 0) return(final)
-  }
+}
 
 
 
