f1833c2a2dd9797128c91901339106383fef5617
Jari Oksanen <jari.oksanen@oulu.fi>
2012-02-29 10:33:53 +0200

    add lines.traj and make plot.traj 'type' configurable

--- R/traj.R
+++ R/traj.R
@@ -10,8 +10,14 @@
 
 `plot.traj` <-
     function(x, xlab = "Time (t)", ylab = "Population size", lwd =2, lty = 1,
-             col =c(2,4), ...)
+             col =c(2,4), type = "l", ...)
 {
     matplot(x[,1], x[,-1], xlab = xlab, ylab = ylab, lwd = lwd, lty = lty,
-            col = col, type = "l", ...) 
+            col = col, type = type, ...) 
+}
+
+`lines.traj` <-
+    function(x, ...)
+{
+    matlines(x[,1], x[,-1], ...)
 }
