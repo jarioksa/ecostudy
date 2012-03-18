678a289f73162eb080948715e21a09bf6b8c60df
Jari Oksanen <jari.oksanen@oulu.fi>
2012-03-18 08:32:31 +0200

    Add first rudiments of Tilman competition model
    The equations were based on Populus help documents. The numeric results
    conflict with the source: we get population sizes that are 2/3 of the
    ones we should get, but critical resource levels (R*) are OK.

---
+++ R/tilman.R
@@ -0,0 +1,31 @@
+### Tilman Resource Competition model
+
+`tilman` <-
+    function()
+{
+}
+
+## parameters for number of species [S] and resources [R]
+## N[S]: population sizes
+## R[R]: amount of resources
+## r[S]: species growth rate
+## k[R,S]: half-saturation constants
+## m[S]: mortality
+## S[R]: resource supply point
+## a[R]: resource mobilation rate
+## c[R,S]: consumption rate
+
+odeTilman <-
+    function(t, y, p, ...)
+{
+    Ry <- y[1:p$nres]
+    Ny <- y[-(1:p$nres)]
+    up <- outer(Ry, p$r, "*")
+    down <- sweep(p$k, 1, Ry, "+")
+    dN.Ndt <- up/down
+    dR.dt1 <- p$a * (p$S - Ry) 
+    dR.dt2 <- rowSums(sweep(p$c * sweep(dN.Ndt, 2, p$m, "+"), 2, Ny, "*"))
+    dR.dt <- dR.dt1 - dR.dt2
+    dN.dt <- Ny * apply(sweep(dN.Ndt, 2, p$m, "-"), 2, min)
+    list(c(dR.dt, dN.dt))
+}
