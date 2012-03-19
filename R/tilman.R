09c64cb04ef0f61137ab3a8fb88e509126cb295e
Jari Oksanen <jari.oksanen@oulu.fi>
2012-03-19 16:41:34 +0200

    The skeleton of tilman() function to set up the model

--- R/tilman.R
+++ R/tilman.R
@@ -1,19 +1,40 @@
 ### Tilman Resource Competition model
 
-`tilman` <-
-    function()
-{
-}
 
 ## parameters for number of species [S] and resources [R]
-## N[S]: population sizes
-## R[R]: amount of resources
+## N[S]: population sizes (starting sizes for populations: traj) 
+## R[R]: amount of resources (staritng size for resource pool: traj)
 ## r[S]: species growth rate
 ## k[R,S]: half-saturation constants
 ## m[S]: mortality
 ## S[R]: resource supply point
 ## a[R]: resource mobilation rate
 ## c[R,S]: consumption rate
+
+`tilman` <-
+    function(S, a, r, m, k, c)
+{
+    ## Interpret number of resources and number of species from input
+    ## parameter
+    nres <- length(S)
+    nsp <- length(r)
+    ## Check or complement other model parameters
+    if (length(a) < nres)
+        a <- rep(a, nres)
+    if (length(m) < nsp)
+        m <- rep(a, nsp)
+    ## Take care that c and k are matrices
+    if (!is.matrix(k))
+        k <- matrix(k, nrow = nres, ncol = nsp, byrow = TRUE)
+    if (!is.matrix(c))
+        c <- matrix(c, nrow = nres, ncol = nsp, byrow = TRUE)
+    ## Model solutions
+    Rstar <- sweep(sweep(k, 2, m, "*"), 2, r-m, "/")
+    Nstar <- a * sweep(-Rstar, 1,  S, "+") / sweep(c, 2, m, "*")
+    list(Rstar = Rstar, Nstar = Nstar)
+}
+
+## Differential equations
 
 odeTilman <-
     function(t, y, p, ...)
