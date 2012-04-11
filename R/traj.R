79b3541cf78bd77f0e0fa9b23f79d86a00a9ec29
Jari Oksanen <jari.oksanen@oulu.fi>
2012-04-11 15:33:37 +0300

    recycle 'col' in plot.traj()

--- R/traj.R
+++ R/traj.R
@@ -12,6 +12,7 @@
     function(x, xlab = "Time (t)", ylab = "Population size", lwd =2, lty = 1,
              col =c(4,2), type = "l", ...)
 {
+    col <- rep(col, length = ncol(x) - 1)
     matplot(x[,1], x[,-1], xlab = xlab, ylab = ylab, lwd = lwd, lty = lty,
             col = col, type = type, ...) 
 }
